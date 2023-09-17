47d1ac7166fa90803027efb2fd9fdd6845fa5a10
omiki235 <canomiki05@gmail.com>
2023-09-17 13:58:19 +0900

    Postsの実装

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const postRouter = require('./routes/posts');
 const mongoose = require('mongoose');
 
 const port = 8000;
+const path = require();
 
-mongoose.connect('mongodb://127.0.0.1/nandemo_db');
+mongoose.connect(process.env.MONGOURL).then(() => {});
 
 const db = mongoose.connection;
 
@@ -13,4 +15,4 @@ db.once('open', () => console.log('DB connection successful'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`${port}ポートが立ち上がりました。`));
