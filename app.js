ec359b58adcdb948dc464a63c473a6c69d181ea3
omiki235 <canomiki05@gmail.com>
2023-09-18 15:12:58 +0900

    upload.js create

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 const postRouter = require('./routes/posts');
+const uploadRouter = require('./routes/upload');
 const mongoose = require('mongoose');
-
 const port = 8000;
-const path = require();
+const path = require('path');
 
-mongoose.connect(process.env.MONGOURL).then(() => {});
+//環境変数を読み込む
+require('dotenv').config();
 
-const db = mongoose.connection;
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log('DBと接続中.........');
+  })
+  .catch((err) => {
+    console.log('エラーが発生しました。', err);
+  });
 
-db.on('error', console.error.bind(console, 'DB connection error:'));
-db.once('open', () => console.log('DB connection successful'));
-
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use(express.json());
+app.use('/api/posts', postRouter);
+app.use('/api/upload', uploadRouter);
 
 app.listen(port, () => console.log(`${port}ポートが立ち上がりました。`));
