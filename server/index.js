ace99b47fea4b0572396b7ad3c720a1c39874fac
omiki235 <omikey005@gmail.com>
2024-01-25 16:44:08 +0900

    backend cors fix

--- server/index.js
+++ server/index.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const app = express();
 const PORT = 8000;
 const pool = require('./src/config/db.config');
 
+// 環境変数を読み込む
 require('dotenv').config();
 
+// CORS設定の詳細
 const corsOptions = {
-  cors: {
-    origin: '*',
-  },
-  optionsSuccessStatus: 200,
-  credentials: true,
+  allowedHeaders: ['sessionId', 'Content-Type'],
+  exposedHeaders: ['sessionId'],
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
 };
 
+// CORSミドルウェアを適用
 app.use(cors(corsOptions));
 
+// JSONボディパーサーを適用
 app.use(express.json());
 
+// APIのルートを設定
 app.use('/api', require('./src/routes'));
-app.use('*', express.static(path.join(__dirname, 'build')));
+
+// アップロードされたファイルを提供するためのルートを設定
+app.use('/uploads', express.static('./src/uploads'));
+
+// サーバーを起動
 app.listen(PORT, () => {
   console.log(`${PORT}番のサーバーが起動しました`);
 });
 
-app.use('/uploads', express.static('./src/uploads'));
-
+// データベース接続を確認
 pool
   .getConnection()
   .then((connection) => {
